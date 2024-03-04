b37c03c2b6307462dd22fd1b0d069a201df9fd44
Iaskasan <nokiesan@yahoo.fr>
2024-03-04 16:19:14 +0100

    finished the base model task

--- models/base_models.py
+++ models/base_models.py
@@ -9,14 +9,19 @@ class BaseModel:
     def __init__(self):
         """init instances"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime().utcnow
-        self.updated_at = datetime().utcnow
+        self.created_at = datetime().now()
+        self.updated_at = datetime().now()
 
     def __str__(self):
         print(f"[{__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        
+        self.updated_at = datetime().now()
     
     def to_dict(self):
-        pass
+        """Return dictionary representation of BaseModel instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
