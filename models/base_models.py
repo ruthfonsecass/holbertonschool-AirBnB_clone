8cafae3dfc8eb2aa9fb76ea2d2fb57fb6cb22613
Iaskasan <nokiesan@yahoo.fr>
2024-03-05 11:21:34 +0100

    added args and kwargs to init function and fixed some pycodestyle

--- models/base_models.py
+++ models/base_models.py
@@ -4,20 +4,28 @@ from datetime import datetime
 
 class BaseModel:
     """Base model class"""
-    
-    
+
     def __init__(self, *args, **kwargs):
-        """init instances"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        value =\
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """updates the public instance attribute with the current datetime"""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         """Return dictionary representation of BaseModel instance."""
         obj_dict = self.__dict__.copy()
@@ -26,4 +34,3 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
