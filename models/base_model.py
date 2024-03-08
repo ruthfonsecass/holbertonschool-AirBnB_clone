718ef8e2af980a9d446fda8d141a64400e9a759a
Iaskasan <nokiesan@yahoo.fr>
2024-03-08 19:48:16 +0100

    removed tests

--- models/base_model.py
+++ models/base_model.py
@@ -35,16 +35,3 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
