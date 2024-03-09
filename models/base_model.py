efc146a442faf74686585d4b4c153994001be3c8
ruthfonsecass <7502@holbertonstudents.com>
2024-03-09 16:13:43 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 
@@ -11,12 +11,13 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ('created_at', 'updated_at'):
-                        value =\
-                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
+                        if type(value) is str:
+                            value =\
+                                datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, key, value)
 
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
