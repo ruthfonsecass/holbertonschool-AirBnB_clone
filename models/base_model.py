62691a5fcb53b012b0ac0f04b8f30e23a3d25ef2
ruthfonsecass <7502@holbertonstudents.com>
2024-03-25 10:02:52 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self,  *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
