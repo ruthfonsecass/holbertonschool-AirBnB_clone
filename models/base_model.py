ce9cff1e6c25cd94c6de9a3d716708eaa6a2e379
ruthfonsecass <7502@holbertonstudents.com>
2024-03-25 11:08:39 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,24 @@ from models import storage
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self,  *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs: 
+            for key, value in kwargs.items(): 
+                if key == 'created_at' or key == 'updated_at': 
+                    if type(value) is str: 
+                        value = datetime.strptime( 
+                            value, "%Y-%m-%dT%H:%M:%S.%f") 
+
+  
+
+                if key != '__class__': 
+
+                    setattr(self, key, value)
+
+        else:           
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
         storage.new(self)
 
     def __str__(self):
